034bb560301f631cbebfaa1efca1f27a4d845eb4
Damian Poloczek <damianpoloczek@gmail.com>
2025-11-11 22:47:51 +0100

    about_me.html removed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about_me.html"),
         termin: resolve(__dirname, "termin.html"),
         datenschutz: resolve(__dirname, "datenschutz.html"),
         impressum: resolve(__dirname, "impressum.html"),
