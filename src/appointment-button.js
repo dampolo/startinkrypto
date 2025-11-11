1ba90fec0070cf294baadc5d0f3007366cff002f
Damian Poloczek <damianpoloczek@gmail.com>
2025-11-11 21:49:34 +0100

    change from appointment to termin and changed the address

--- src/appointment-button.js
+++ src/appointment-button.js
@@ -9,7 +9,8 @@ class AppointmentButton extends HTMLElement {
     // Create the wrapper
     const wrapper = document.createElement("a");
     wrapper.className = "main-button";
-    wrapper.href = "appointment.html";
+
+    wrapper.href = "termin.html";
 
     // Insert SVG
     wrapper.innerHTML = `
